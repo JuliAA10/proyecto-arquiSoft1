b600a363c9c5aed97c11a3a9b0c69818b68fe13a
JuliAA10 <110618330+JuliAA10@users.noreply.github.com>
2024-06-13 15:01:39 -0300

    Comments del código

--- server/models/Student.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const studentSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-const studentModel = mongoose.model("Student", studentSchema);
-export { studentModel as Student };
